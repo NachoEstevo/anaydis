1b2b162eeaf6671aa6d9e1ab4a4586bdaf4fc002
Ignacio Estevo <ignacio.estevo@ing.austral.edu.ar>
2021-09-01 20:25:09 -0300

    Added Quick, Shell, HSorter and respective tests

--- src/main/java/anaydis/sort/HSorter.java
+++ src/main/java/anaydis/sort/HSorter.java
@@ -5,30 +5,29 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Comparator;
 import java.util.List;
 
-public class HSorter extends AbstractSorter {
-
+public class HSorter extends AbstractSorter
+{
     public HSorter() {
         super(SorterType.H);
     }
 
-    public <T> void sort(@NotNull Comparator<T> comparator, @NotNull List<T> list) {
-
-        sort(comparator, list, 1);//I use 1 to be used later in the ShellSorter
+    public <T> void sort(@NotNull Comparator<T> comparator, @NotNull List<T> list){
+        sort(comparator, list, 1);
     }
 
-    <T> void sort(Comparator<T> comparator, List<T> list, int i) {
-
-        int h = 4;
-        for (i = h; i <= list.size()-1; i++) {
+    public <T> void sort(Comparator<T> comparator, List<T> list, int h)
+    {
+        for (int i = h; i < list.size() ; i++) {
             T data = list.get(i);
             int j = i;
-            while (j >= h && greater(list.get(j-h),data, comparator)) {
-                list.set(j, list.get(j - h));
+            while (j >= h && greater(list.get(j-h) , data , comparator)){
+                list.set(j , list.get(j-h));
                 j -= h;
             }
-            list.set(j, data);
+            list.set(j ,data);
         }
-
     }
+
+
 }
 
