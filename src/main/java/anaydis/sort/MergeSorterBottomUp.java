438eaa73f32dc9582af9d9edd7e079d8bb31988b
Ignacio Estevo <ignacio.estevo@ing.austral.edu.ar>
2021-09-05 19:34:28 -0300

    Added Bottom Up

--- src/main/java/anaydis/sort/MergeSorterBottomUp.java
+++ src/main/java/anaydis/sort/MergeSorterBottomUp.java
@@ -11,7 +11,16 @@ public class MergeSorterBottomUp extends AbstractSorter{
 
 
     @Override
-    public <T> void sort(@NotNull Comparator<T> comparator, @NotNull List<T> list) {
+    public <T> void sort(@NotNull Comparator<T> comparator, @NotNull List<T> list) {bottomUp(list,0,list.size()-1,comparator);}
 
+    private <T>void bottomUp(List<T> list, int low, int high,Comparator<T> comparator){
+
+        for(int middle = 1; middle <= high - low; middle *= 2) {
+            final int middleX2 = middle * 2;
+            for (int low1 = low; low1 <= high - middle; low1 += middleX2) {
+                final int high1 = Math.min(low1 - low + middleX2 - 1, high);
+                merge(list, low1, low1 + middle - 1, high1,comparator);
+            }
+        }
     }
 }
--- src/test/java/anaydis/sort/TestPractice02.java
+++ src/test/java/anaydis/sort/TestPractice02.java
@@ -139,7 +139,18 @@ public class TestPractice02 extends SorterTest {
         testSorter(createIntegerDataSetGenerator(), SorterType.MERGE_TOP_DOWN, 50);
         testSorter(createIntegerDataSetGenerator(), SorterType.MERGE_TOP_DOWN, 100);
     }
-
+    /** Test MergeSorter TopDown with String generator. */
+    @Test public void testMergeBottomUpWithStringGenerator() {
+        testSorter(createStringDataSetGenerator(), SorterType.MERGE_BOTTOM_UP, 10);
+        testSorter(createStringDataSetGenerator(), SorterType.MERGE_BOTTOM_UP, 50);
+        testSorter(createStringDataSetGenerator(), SorterType.MERGE_BOTTOM_UP, 100);
+    }
+    /** Test MergeSorter TopDown with String generator. */
+    @Test public void testMergeBottomUpWithIntegerGenerator() {
+        testSorter(createIntegerDataSetGenerator(), SorterType.MERGE_BOTTOM_UP, 10);
+        testSorter(createIntegerDataSetGenerator(), SorterType.MERGE_BOTTOM_UP, 50);
+        testSorter(createIntegerDataSetGenerator(), SorterType.MERGE_BOTTOM_UP, 100);
+    }
 
 
 
