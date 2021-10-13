4e9b37c2bab93a86de8c455713b964cc4845ee52
Ignacio Estevo <ignacio.estevo@ing.austral.edu.ar>
2021-10-13 19:35:36 -0300

    BankersQueue implementation

--- src/main/java/anaydis/immutable/BankersQueue.java
+++ src/main/java/anaydis/immutable/BankersQueue.java
@@ -1,16 +1,38 @@
 package anaydis.immutable;
 
-public class BankersQueue<T> {
+import org.jetbrains.annotations.NotNull;
 
+public class BankersQueue<T> implements Queue<T> { //Se manejan 2 listas.
 
+    private final List<T> in;
+    private final List<T> out;
 
-    public void enqueue(T value) {
-
+    public BankersQueue(@NotNull List<T> in, @NotNull List<T> out){
+        this.in = in;
+        this.out = out;
     }
 
-    public T dequeue() {
-        return null;
+    BankersQueue() {
+        this.in = List.nil();
+        this.out = List.nil();
+    }
+
+    @Override
+    public @NotNull BankersQueue<T> enqueue(@NotNull T value) {return new BankersQueue<>(List.cons(value, in), out);}
+
+
+    @Override
+    public @NotNull Result<T> dequeue() {
+        if (out.isEmpty()){
+            List<T> out = in.reverse();
+            return new Result<>(out.head(), new BankersQueue<>(List.nil(), out.tail()));
+        }
+        return new Result<>(out.head(), new BankersQueue<>(in, out.tail()));
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return in.isEmpty() && out.isEmpty();
     }
 
 }
-
